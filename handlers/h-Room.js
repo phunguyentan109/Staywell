b55fd4db793923f107b52046a79809d1e56dc1cc
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2019-12-25 14:58:47 +0700

    Working on manage room

--- handlers/h-Room.js
+++ handlers/h-Room.js
@@ -54,78 +54,77 @@ exports.remove = async(req, res, next) => {
 exports.update = async(req, res, next) => {
     try{
         const {room_id} = req.params;
-        let {name, desc, people_id, price_id} = req.body;
-        people_id = people_id.map(p => p._id);
+        let {name, price_id, user_id} = req.body;
         let foundRoom = await db.Room.findById(room_id);
 
         // remove old people and add new people to the room
-        let oldPeople = foundRoom.people_id.filter(id => people_id.indexOf(id) === -1);
-        let curPeople = foundRoom.people_id.filter(id => people_id.indexOf(id) !== -1);
-        let newPeople = people_id.filter(id => curPeople.indexOf(id) === -1);
+        const NOT_FOUND = -1;
+        let oldUser = foundRoom.people_id.filter(id => user_id.indexOf(id) === NOT_FOUND);
+        let curUser = foundRoom.people_id.filter(id => user_id.indexOf(id) !== NOT_FOUND);
+        let newUser = user_id.filter(id => curUser.indexOf(id) === NOT_FOUND);
 
         // remove room id of old people
-        for(let id of oldPeople) {
-            await assignId("People", id, "room_id", false);
+        for(let id of oldUser) {
+            await assignId("User", id, "room_id", false);
 
             // send mail to notify people about removing from the room
-            let foundPeople = await db.People.findById(id).populate("user_id").exec();
-            let {email, viewname} = foundPeople.user_id;
+            let foundUser = await db.User.findById(id).lean().exec();
+            let {email, viewname} = foundUser;
             await mail.leaveRoom(email, viewname, foundRoom.name);
         }
 
         // assign room id for new people
-        for(let id of newPeople) {
-            await assignId("People", id, "room_id", foundRoom._id);
+        for(let id of newUser) {
+            await assignId("User", id, "room_id", foundRoom._id);
 
             // send mail to notify people about new place
-            let foundPeople = await db.People.findById(id).populate("user_id").exec();
-            let {email, viewname} = foundPeople.user_id;
+            let foundUser = await db.User.findById(id).lean().exec();
+            let {email, viewname} = foundUser;
             await mail.getRoom(email, viewname, foundRoom.name);
         }
 
-        let updatePeople_id = [...curPeople, ...newPeople];
-        let billList = [];
-        if(updatePeople_id.length > 0) {
-            // Generate bill timeline in case the room is in used after a time
-            if(foundRoom.people_id.length === 0) {
-                let price = await db.Price.findById(foundRoom.price_id);
-                for(let i = 1; i <= price.duration; i++) {
-                    let bill = await db.Bill.create({
-                        pay: {
-                            time: moment().add(i, "M")
-                        },
-                        room_id: foundRoom._id
-                    })
-                    billList.push(bill._id);
-                }
-            }
-        } else {
-            // Removing bill timeline in case the room is empty after editing
-            if(foundRoom.people_id.length !== 0) {
-                let foundBills = await db.Bill.find({room_id: foundRoom._id, water: 0});
-                let foundBill_ids = foundBills.map(v => v._id);
-                await casDeleteMany("Bill", foundBill_ids);
-
-                // close contract (no active bill)
-                await db.Bill.updateMany({room_id: foundRoom._id, inContract: true}, {inContract: false, "pay.status": true});
-            }
-        }
+        let updateUser_id = [...curUser, ...newUser];
+        // let billList = [];
+        // if(updateUser_id.length > 0) {
+        //     // Generate bill timeline in case the room is in used after a time
+        //     if(foundRoom.people_id.length === 0) {
+        //         let price = await db.Price.findById(foundRoom.price_id);
+        //         for(let i = 1; i <= price.duration; i++) {
+        //             let bill = await db.Bill.create({
+        //                 pay: {
+        //                     time: moment().add(i, "M")
+        //                 },
+        //                 room_id: foundRoom._id
+        //             })
+        //             billList.push(bill._id);
+        //         }
+        //     }
+        // } else {
+        //     // Removing bill timeline in case the room is empty after editing
+        //     if(foundRoom.people_id.length !== 0) {
+        //         let foundBills = await db.Bill.find({room_id: foundRoom._id, water: 0});
+        //         let foundBill_ids = foundBills.map(v => v._id);
+        //         await casDeleteMany("Bill", foundBill_ids);
+        //
+        //         // close contract (no active bill)
+        //         await db.Bill.updateMany({room_id: foundRoom._id, inContract: true}, {inContract: false, "pay.status": true});
+        //     }
+        // }
 
         // Room data has been modified so we need to retrieve room data again
-        let updatedRoom = await db.Room.findById(room_id);
+        // let updatedRoom = await db.Room.findById(room_id);
+
         // update room
-        updatedRoom.bill_id = billList;
-        updatedRoom.people_id = updatePeople_id;
-        updatedRoom.name = name;
-        updatedRoom.desc = desc;
-        if(updatedRoom.price_id !== price_id) {
+        foundRoom.user_id = updateUser_id;
+        foundRoom.name = name;
+        if(foundRoom.price_id.equals(price_id)) {
             await spliceId("Price", updatedRoom.price_id, "room_id", updatedRoom._id);
             updatedRoom.price_id = price_id;
             await pushId("Price", price_id, "room_id", updatedRoom._id);
         }
-        await updatedRoom.save();
+        foundRoom = await foundRoom.save();
 
-        return res.status(200).json(updatedRoom);
+        return res.status(200).json(foundRoom);
     } catch(err){
         return next(err);
     }
