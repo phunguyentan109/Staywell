6924d636f40102ac505c596d1d8a53a7f40bc87f
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-01-25 19:38:21 +0700

    #10-Fix-error-response

--- handlers/h-Error.js
+++ handlers/h-Error.js
@@ -1,4 +1,10 @@
-exports.handle = (err, req, res) => {
+exports.invalidRoute = (req, res, next) => {
+    let err = new Error("Route not found!");
+    err.status = 404;
+    return next(err);
+}
+
+exports.wrapErr = (err, req, res, next) => {
     return res.status(err.status || 500).json({
         errorMsg: err.message || "Oops! Something went wrong!"
     })
