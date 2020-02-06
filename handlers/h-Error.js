4ed916f2bc66042b996cdbe9293289c0d5b43407
HieuTQ <truongquanghieu8198@gmail.com>
2020-02-06 19:46:50 +0700

    rebase review

--- handlers/h-Error.js
+++ handlers/h-Error.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 exports.invalidRoute = (req, res, next) => {
     let err = new Error("Route not found!");
     err.status = 404;
@@ -6,10 +5,6 @@ exports.invalidRoute = (req, res, next) => {
 }
 
 exports.wrapErr = (err, req, res, next) => {
-=======
-exports.handle = (err, req, res) => {
-    console.log(err);
->>>>>>> test message
     return res.status(err.status || 500).json({
         errorMsg: err.message || "Oops! Something went wrong!"
     })
