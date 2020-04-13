c7102a835eb91083976a7693eedb58840cfda3df
phunguyentan109 <phulicon815490@gmail.com>
2020-04-13 08:39:12 +0700

    Working on rebuild the UI

--- cli/src/views/App/Contract/components/Contract.jsx
+++ cli/src/views/App/Contract/components/Contract.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, Fragment } from 'react'
 import { Card, Spin, Form, Input, Button, Table, Row, Col } from 'antd'
 import { apiContract, apiBill, apiRoom } from 'constants/api'
+import ContainerHeader from 'components/ContainerHeader'
+import PropTypes from 'prop-types'
 import moment from 'moment'
 
 export default function Contract({ notify, hdCancel, room, match, loading, setLoading }) {
