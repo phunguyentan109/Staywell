1480164389a3cc1b78bb3e3fb0d7f8e9b62350ee
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-04-25 18:08:32 +0700

    Working on contract backend

--- models/m-Bill.js
+++ models/m-Bill.js
@@ -1,42 +1,25 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 // const {spliceId} = require("../utils/dbSupport");
 // const db = require("../models");
 
 const billSchema = new mongoose.Schema({
-    electric: {
-        type: Number,
-        default: 0
-    },
-    house: {
-        type: Number,
-        default: 0
-    },
-    water: {
-        type: Number,
-        default: 0
-    },
-    wifi: {
-        type: Number,
-        default: 0
-    },
-    contract_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Contract"
-    },
-    isPaid: {
-        type: Boolean,
-        default: false
-    },
-    paidDate: Date
-}, { timestamps: true });
+  electric: {
+    number: { type: Number, default: 0 },
+    amount: Number,
+    cost: Number
+  },
+  living: Number,
+  water: Number,
+  wifi: Number,
+  contract_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Contract'
+  },
+  payment: {
+    state: { type: Boolean, default: false },
+    at: Date
+  },
+  deadline: { type: Date, required: true }
+}, { timestamps: true })
 
-billSchema.pre("remove", async function(next) {
-    try {
-        // await casDeleteMany("TimePoint", this.timePoint_id);
-        return next();
-    } catch (e) {
-        return next(e);
-    }
-})
-
-module.exports = mongoose.model("Bill", billSchema);
+module.exports = mongoose.model('Bill', billSchema)
