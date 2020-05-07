78ea22a93a5e63292935cca2287f442b2824f706
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-05-07 21:04:25 +0700

    #25 Refactor code

---
+++ cli/src/hocs/withHelpers.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { MOBILE_SIZE, DESKTOP_SIZE } from 'constants/variables'
+import { notification } from 'antd'
+
+const DEFAULT_DESC = 'There may be some errors occuring in the process. Please wait and try again later.'
+
+export default function withHelpers(WrappedComponent) {
+  function Helpers({ ...props }) {
+    const [device, setDevice] = useState({
+      isMobile: false,
+      isTablet: false,
+      isDesktop: true
+    })
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      hdResize()
+      window.addEventListener('resize', hdResize)
+      return () => window.removeEventListener('resize', hdResize)
+    }, [])
+
+    function hdResize() {
+      let isMobile = window.innerWidth < MOBILE_SIZE
+      let isTablet = window.innerWidth >= MOBILE_SIZE && window.innerWidth < DESKTOP_SIZE
+      let isDesktop = window.innerWidth >= DESKTOP_SIZE
+      setDevice({ isMobile, isTablet, isDesktop })
+    }
+
+    const openNotificationWithIcon = (type, message, description=DEFAULT_DESC) => {
+      notification[type]({ message, description })
+    }
+
+    return <WrappedComponent
+      device={{ ...device }}
+      notify={openNotificationWithIcon}
+      loading={loading}
+      setLoading={setLoading}
+      {...props}
+    />
+  }
+
+  return Helpers
+}
--- test/sample.js
+++ test/sample.js
@@ -1,42 +1,41 @@
-const db = require("../models");
-const {casDelete} = require("../utils/dbSupport");
+const { casDelete } = require('../utils/dbSupport')
 
 exports.user = {
-    email: "staywellsystem7@gmail.com",
-    password: "test"
+  email: 'staywellsystem7@gmail.com',
+  password: 'test'
 }
 
 exports.owner = {
-    email: process.env.GMAILUSER,
-    password: "owner"
+  email: process.env.GMAILUSER,
+  password: 'owner'
 }
 
 exports.price = {
-    electric: 2232,
-    wifi: 1,
-    water: 50,
-    house: 2000,
-    extra: 70,
-    type: "priceTest",
-    duration: 6
+  electric: 2232,
+  wifi: 1,
+  water: 50,
+  house: 2000,
+  extra: 70,
+  type: 'priceTest',
+  duration: 6
 }
 
 exports.bill = {
-    amount: 101
+  amount: 101
 }
 
 exports.room = {
-    name: "Room 1",
-    desc: "Wide: 10, Long: 5"
+  name: 'Room 1',
+  desc: 'Wide: 10, Long: 5'
 }
 
 exports.clear = async() => {
-    try {
-        await casDelete("User", "email", exports.user.email);
-        await casDelete("Room", "name", exports.room.name);
-        await casDelete("Price", "type", exports.price.type);
-        await casDelete("Bill", "amount", exports.bill.amount);
-    } catch(err) {
-        console.log(err);
-    }
+  try {
+    await casDelete('User', 'email', exports.user.email)
+    await casDelete('Room', 'name', exports.room.name)
+    await casDelete('Price', 'type', exports.price.type)
+    await casDelete('Bill', 'amount', exports.bill.amount)
+  } catch(err) {
+    console.log(err)
+  }
 }
