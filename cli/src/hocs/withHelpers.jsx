6cf58f7a5fa50573ddc353a6c3211ce6cb8e5afb
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-05-10 16:29:41 +0700

    #40 Implement whole page loading

--- cli/src/hocs/withHelpers.jsx
+++ cli/src/hocs/withHelpers.jsx
@@ -1,43 +1,46 @@
-import React, { useState, useEffect } from "react";
-import { MOBILE_SIZE, DESKTOP_SIZE } from "constants/variables";
-import { notification } from "antd";
+import React, { useState, useEffect } from 'react'
+import { MOBILE_SIZE, DESKTOP_SIZE } from 'constants/variables'
+import { notification, Spin } from 'antd'
 
-const DEFAULT_DESC = "There may be some errors occuring in the process. Please wait and try again later.";
+const DEFAULT_DESC = 'There may be some errors occuring in the process. Please wait and try again later.'
 
 export default function withHelpers(WrappedComponent) {
-    function Helpers({ ...props }) {
-        const [device, setDevice] = useState({
-            isMobile: false,
-            isTablet: false,
-            isDesktop: true
-        });
-        const [loading, setLoading] = useState(true);
+  function Helpers({ ...props }) {
+    const [device, setDevice] = useState({
+      isMobile: false,
+      isTablet: false,
+      isDesktop: true
+    })
+    const [loading, setLoading] = useState(true)
 
-        useEffect(() => {
-            hdResize();
-            window.addEventListener("resize", hdResize);
-            return () => window.removeEventListener("resize", hdResize);
-        }, []);
+    useEffect(() => {
+      hdResize()
+      window.addEventListener('resize', hdResize)
+      return () => window.removeEventListener('resize', hdResize)
+    }, [])
 
-        function hdResize() {
-            let isMobile = window.innerWidth < MOBILE_SIZE;
-            let isTablet = window.innerWidth >= MOBILE_SIZE && window.innerWidth < DESKTOP_SIZE;
-            let isDesktop = window.innerWidth >= DESKTOP_SIZE;
-            setDevice({ isMobile, isTablet, isDesktop });
-        }
-
-        const openNotificationWithIcon = (type, message, description=DEFAULT_DESC) => {
-            notification[type]({ message, description });
-        };
-
-        return <WrappedComponent
-            device={{ ...device }}
-            notify={openNotificationWithIcon}
-            loading={loading}
-            setLoading={setLoading}
-            {...props}
-        />
+    function hdResize() {
+      let isMobile = window.innerWidth < MOBILE_SIZE
+      let isTablet = window.innerWidth >= MOBILE_SIZE && window.innerWidth < DESKTOP_SIZE
+      let isDesktop = window.innerWidth >= DESKTOP_SIZE
+      setDevice({ isMobile, isTablet, isDesktop })
     }
 
-    return Helpers;
+    const openNotificationWithIcon = (type, message, description=DEFAULT_DESC) => {
+      notification[type]({ message, description })
+    }
+
+    return (
+      <Spin spinning={loading} size='large'>
+        <WrappedComponent
+          device={{ ...device }}
+          notify={openNotificationWithIcon}
+          setLoading={setLoading}
+          {...props}
+        />
+      </Spin>
+    )
+  }
+
+  return Helpers
 }
