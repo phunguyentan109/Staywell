49fd05ba3081cbdb956a6e560a601874ee9cb4ea
phunguyentan109 <phulicon815490@gmail.com>
2020-07-06 08:44:01 +0700

    Change room api

--- cli/src/constants/api/models/room.js
+++ cli/src/constants/api/models/room.js
@@ -1,10 +1,28 @@
 import { apiCall, spec } from '../call'
 const common = '/api/rooms'
 
-export const get = async(id) => await apiCall('get', `${common}${spec(id)}`)
-export const create = async(room) => await apiCall('post', common, room)
-
-export const getOne = async(id) => await apiCall('get', `${common}/${id}`)
-export const remove = async(id) => await apiCall('delete', `${common}/${id}`)
-export const update = async(id, data) => await apiCall('put', `${common}/${id}`, data)
-export const assign = async(id, data) => await apiCall('put', `${common}/${id}/assign`, data)
+export default async function (name, { data, params } = {}, throwErr) {
+  let config, { room_id } = params
+  switch (name) {
+    case 'get': {
+      config = { url: spec(room_id) }
+      break
+    }
+    case 'remove': {
+      config = { method: 'delete', url: `/${room_id}` }
+      break
+    }
+    case 'update': {
+      config = { url: `/${room_id}` }
+      break
+    }
+    case 'assign': {
+      config = { url: `/${room_id}/assign` }
+      break
+    }
+    default: config = { url: '' }
+  }
+  // Call api
+  config.url = common + config.url
+  return await apiCall({ ...config, data }, throwErr)
+}
