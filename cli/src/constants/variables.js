89c367098838fe93c874d44a8e0ffb0b20a47fab
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-08-09 22:47:51 +0700

    Working on new ui

--- cli/src/constants/variables.js
+++ cli/src/constants/variables.js
@@ -3,19 +3,19 @@ import { HomeOutlined } from '@ant-design/icons'
 export const breadNames = {
   '/app': {
     name: 'Dashboard',
-    icon: HomeOutlined,
+    icon: HomeOutlined
   },
   '/app/people': {
-    name: 'Manage People',
+    name: 'Manage People'
   },
   '/app/price': {
-    name: 'Manage Price',
+    name: 'Manage Price'
   },
   '/app/profile': {
-    name: 'Manage Profile',
+    name: 'Manage Profile'
   },
   '/app/rooms': {
-    name: 'Manage Room',
+    name: 'Manage Room'
   },
   '/app/contracts': {
     name: 'Manage Contract'
