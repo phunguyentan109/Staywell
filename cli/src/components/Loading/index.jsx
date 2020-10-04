3ff578f6d3f69397205bed6a871d622b0d070636
quang hieu <truongquanghieu8198@gmail.com>
2020-10-04 23:46:45 +0700

    Implement loading for Profile

---
+++ cli/src/components/Loading/index.jsx
@@ -0,0 +1,20 @@
+import React, { useImperativeHandle, forwardRef } from 'react'
+import { Spin } from 'antd'
+import { useToggle } from 'hooks'
+import PropTypes from 'prop-types'
+
+function Loading(props, ref) {
+  const [state, toggleState] = useToggle({ loading: false })
+  
+  useImperativeHandle(ref, () => ({
+    toggle: () => toggleState()
+  }))
+
+  return <Spin spinning={state.loading} size='large'>{props.children}</Spin>
+}
+
+Loading.propTypes = {
+  children: PropTypes.any,
+}
+
+export default forwardRef(Loading)
