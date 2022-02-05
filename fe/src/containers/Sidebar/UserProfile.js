94c5855d67f7264a940f37a84f81ab923800f888
phunguyentan109 <phulicon815490@gmail.com>
2022-02-05 12:39:38 +0700

    Done
    Done
    
    Done

--- fe/src/containers/Sidebar/UserProfile.js
+++ fe/src/containers/Sidebar/UserProfile.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Avatar, Popover } from 'antd'
+import React, { useMemo } from 'react'
+import { Popover } from 'antd'
+import Avatar, { genConfig } from 'react-nice-avatar'
+import PropTypes from 'prop-types'
+import './_styles.less'
 
-const UserProfile = () => {
-  const userMenuOptions = (
-    <ul className='gx-user-popover'>
-      <li>My Account</li>
-      <li>Connections</li>
-    </ul>
-  )
+const UserProfile = (props) => {
+  const avaConfig = useMemo(() => genConfig(props.avatar), [props.avatar])
 
   return (
-    <div className='gx-flex-row gx-align-items-center gx-mb-4 gx-avatar-row'>
-      <Popover placement='bottomRight' content={userMenuOptions} trigger='click'>
-        <Avatar src='https://via.placeholder.com/150' className='gx-size-40 gx-pointer gx-mr-3' alt=''/>
+    <div className='gx-flex-row gx-align-items-center gx-mb-4 gx-avatar-row userProfile'>
+      <Popover
+        placement='bottomRight'
+        content={
+          <ul className='gx-user-popover'>
+            <li>My Account</li>
+            <li>Connections</li>
+          </ul>
+        }
+        trigger='click'
+      >
+        <Avatar
+          className='gx-size-40 gx-pointer gx-mr-3 customAvatar'
+          {...avaConfig}
+        />
         <span className='gx-avatar-name'>Rob Farnandies<i className='icon icon-chevron-down gx-fs-xxs gx-ml-2'/></span>
       </Popover>
     </div>
   )
+}
+
+UserProfile.propTypes = {
+  avatar: PropTypes.object,
 }
 
 export default UserProfile
--- seed/samples/user.js
+++ seed/samples/user.js
@@ -3,6 +3,23 @@ exports.manager = {
   password: 'manager',
   username: 'manager',
   gender: 'Male',
+  avatar: {
+    'sex': 'woman',
+    'faceColor': '#F9C9B6',
+    'earSize': 'big',
+    'eyeStyle': 'circle',
+    'noseStyle': 'short',
+    'mouthStyle': 'smile',
+    'shirtStyle': 'polo',
+    'glassesStyle': 'round',
+    'hairColor': '#F48150',
+    'hairStyle': 'womanLong',
+    'hatStyle': 'none',
+    'hatColor': '#506AF4',
+    'eyeBrowStyle': 'upWoman',
+    'shirtColor': '#77311D',
+    'bgColor': 'linear-gradient(45deg, #178bff 0%, #ff6868 100%)'
+  }
 }
 
 exports.managerRole = {
@@ -15,28 +32,113 @@ exports.people = [
     email: 'magazine.group@gmail.com',
     username: 'magazine.group',
     gender: 'Male',
-    isVerified: true
+    isVerified: true,
+    avatar: {
+      'sex':'man',
+      'faceColor':'#F9C9B6',
+      'earSize':'small',
+      'eyeStyle':'oval',
+      'noseStyle':'long',
+      'mouthStyle':'smile',
+      'shirtStyle':'polo',
+      'glassesStyle':'none',
+      'hairColor':'#000',
+      'hairStyle':'womanLong',
+      'hatStyle':'none',
+      'hatColor':'#343434',
+      'eyeBrowStyle':'up',
+      'shirtColor':'#358dd8',
+      'bgColor':'linear-gradient(45deg, #1729ff 0%, #ff56f7 100%)'
+    }
   },
   {
     email: 'nothinglike1@gmail.com',
     username: 'nothinglike1',
     gender: 'Male',
-    isVerified: true
+    isVerified: true,
+    avatar: {
+      'sex': 'man',
+      'faceColor': '#F9C9B6',
+      'earSize': 'small',
+      'eyeStyle': 'oval',
+      'noseStyle': 'short',
+      'mouthStyle': 'smile',
+      'shirtStyle': 'hoody',
+      'glassesStyle': 'none',
+      'hairColor': '#000',
+      'hairStyle': 'normal',
+      'hatStyle': 'beanie',
+      'hatColor': '#D2EFF3',
+      'eyeBrowStyle': 'up',
+      'shirtColor': '#595959',
+      'bgColor': 'linear-gradient(45deg, #56b5f0 0%, #45ccb5 100%)'
+    }
   },
   {
     email: 'great121@gmail.com',
     username: 'great121',
     gender: 'Male',
-    isVerified: true
+    isVerified: true,
+    avatar: {
+      'sex': 'man',
+      'faceColor': '#F9C9B6',
+      'earSize': 'small',
+      'eyeStyle': 'oval',
+      'noseStyle': 'round',
+      'mouthStyle': 'smile',
+      'shirtStyle': 'polo',
+      'glassesStyle': 'none',
+      'hairColor': '#000',
+      'hairStyle': 'thick',
+      'hatStyle': 'none',
+      'hatColor': '#000',
+      'eyeBrowStyle': 'up',
+      'shirtColor': '#6BD9E9',
+      'bgColor': 'linear-gradient(45deg, #176fff 0%, #68ffef 100%)'
+    }
   },
   {
     email: 'becauseopu13@gmail.com',
     username: 'becauseopu13',
     gender: 'Female',
+    avatar: {
+      'sex': 'woman',
+      'faceColor': '#F9C9B6',
+      'earSize': 'big',
+      'eyeStyle': 'oval',
+      'noseStyle': 'round',
+      'mouthStyle': 'laugh',
+      'shirtStyle': 'short',
+      'glassesStyle': 'none',
+      'hairColor': '#FC909F',
+      'hairStyle': 'womanLong',
+      'hatStyle': 'none',
+      'hatColor': '#000',
+      'eyeBrowStyle': 'upWoman',
+      'shirtColor': '#6BD9E9',
+      'bgColor': '#D2EFF3'
+    }
   },
   {
     email: 'newbb213@gmail.com',
     username: 'newbb213',
     gender: 'Female',
+    avatar: {
+      'sex': 'woman',
+      'faceColor': '#AC6651',
+      'earSize': 'big',
+      'eyeStyle': 'oval',
+      'noseStyle': 'long',
+      'mouthStyle': 'laugh',
+      'shirtStyle': 'polo',
+      'glassesStyle': 'none',
+      'hairColor': '#000',
+      'hairStyle': 'normal',
+      'hatStyle': 'none',
+      'hatColor': '#F48150',
+      'eyeBrowStyle': 'upWoman',
+      'shirtColor': '#F4D150',
+      'bgColor': '#74D153'
+    }
   }
 ]
